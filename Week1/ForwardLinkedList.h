9846ba84fe7695d940e79f27aeaae4f20ea7cb5b
Telluluu <Tellulu1564959850@gmail.com>
2023-03-22 00:53:41 +0800

    更新了双向链表ADT

--- Week1/ForwardLinkedList.h
+++ Week1/ForwardLinkedList.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iterator>
+#include<iostream>
+
+using namespace std;
 
 //可以考虑让这个类的使用更优雅一点
 //不想折腾的删除这个定义
-#define FLL_ITERATOR_FEATURE ///可选	[迭代器]
+//#define FLL_ITERATOR_FEATURE ///可选	[迭代器]
 
 #ifdef FLL_ITERATOR_FEATURE
 //这个特性要求在插入和移除时 "在形式上" 无需前一个节点，只使用当前节点即可
@@ -11,11 +14,30 @@
 #define FLL_OPTERATE_ON_THIS_FEATURE ///可选 [直接操作]
 #endif
 
-
+template<class T>
+class LNode
+{
+public:
+	T data;
+	LNode<T>* next;
+	LNode<T>()                  //构造函数
+	{
+		data = 0;
+		next = nullptr;
+	}
+	LNode<T>(int i)
+	{
+		data = i;
+		next = nullptr;
+	}
+};
 
 template<class T>
 class ForwardLinkedList
 {
+private:
+	LNode<T>* head = nullptr;
+	int len = 0;
 	//...
 public:
 #ifdef FLL_ITERATOR_FEATURE
@@ -77,5 +99,237 @@ public:
 	void Clear();
 	void Reverse();
 
+	void Insert(int i, int pos);
+	void Erase(int pos);
+	void Print(void);
+	LNode<T>* MidFinder(void);
 	//... other methods
 };
+
+template<class T>
+void ForwardLinkedList<T>::Print()
+{
+	if (head == nullptr)
+	{
+		cout << "Creat a LinkedList at first!" << endl;
+		return;
+	}
+	else if (head->next == nullptr)
+	{
+		cout << "Insert the first Node please" << endl;
+		return;
+	}
+	else 
+	{
+		LNode<T>* pt = head;
+		pt = pt->next;
+		while (pt != nullptr)
+		{
+			cout <<"This Node`s data is:"<< pt->data << endl;
+			pt = pt->next;
+		}
+		return;
+	}
+}
+
+template<class T>
+ForwardLinkedList<T>::ForwardLinkedList()
+{
+	head = new LNode<T>();
+	head->data = 0;
+}
+
+template<class T>
+ForwardLinkedList<T>::~ForwardLinkedList()
+{
+	Clear();
+	//if (head == nullptr)
+	//{
+	//	return;
+	//}
+	//LNode<T>* pt1 = head;   //前指针
+	//LNode<T>* pt2 = pt1;   //后指针
+	//if (pt1->next != nullptr)
+	//{
+	//	pt2 = pt1;
+	//	pt1 = pt1->next;   
+	//	delete(pt2);       //删除前一个结点
+	//}
+	//delete(pt1);         //删除末尾结点
+}
+
+//template<class T>
+//T& ForwardLinkedList<T>::operator[](int i)
+//{
+//	if (i < 0)
+//	{
+//		cout << "请输入一个正整数" << endl;
+//		return;
+//	}
+//	else if (i > size)
+//	{
+//		cout << "链表没有那么长" << endl;
+//		return;
+//	}
+//	else
+//	{
+//		LNode<T>* pt = head;
+//		for (int temp = 0; temp <= i; temp++)
+//		{
+//			pt = pt->next;
+//		}
+//		return T& pt->data;
+//	}
+//}
+
+template<class T>
+void ForwardLinkedList<T>::Insert(int i, int pos)
+{
+	if (pos <= 0)                       //输入不合理
+	{
+		cout << "Please input a number more  than zero" << endl;
+		return;
+	}
+	else if (pos > len + 1)
+	{
+		cout << "input too big" << endl;
+		return;
+	}
+	else
+	{
+		LNode<T>* pt = head;
+		while (pos > 1)
+		{
+			pt = pt->next;   //找到前一个结点
+			pos--;
+		}
+		LNode<T>* newNode=new LNode<T>(i);     //创建新结点
+		newNode->next = pt->next;                        //插入新结点
+		pt->next = newNode;
+	}
+	len++;      //链表长度计数符加1一
+}
+
+template<class T>
+void ForwardLinkedList<T>::Erase(int pos)
+{
+	if (pos <= 0)
+	{
+		cout << "Please input a number which is bigger than zero~" << endl;
+		return;
+	}
+	else if (pos > len)
+		cout << "number input too long!" << endl;
+	else
+	{
+		LNode<T>* pt1 = head;   //找到删除结点
+		LNode<T>* pt2 = pt1;   //用于找到要删除结点的前一个结点
+		while (pt1 != nullptr)
+		{
+			if (pos > 1)
+			{
+				pt1 = pt1->next;   //先让pt1到达删除结点前一个结点
+				pos--;
+			}
+			else break;
+		}
+		pt2 = pt1;         //让pt2到达删除结点前一个结点
+		pt1 = pt1->next;   //pt1到达要删除的结点
+		pt2->next = pt1->next;
+		delete(pt1);
+		cout << "delete successfully" << endl;
+		len--;       //链表长度计数符减一
+	}
+}
+
+//template<class T>
+//T& Front()
+//{
+//
+//}
+//
+//template<class T>
+//T& Back()
+//{
+//
+//}
+
+template<class T>
+void ForwardLinkedList<T>::PushFront(const T& val)
+{
+	Insert(val, 1);      //头插
+}
+
+template<class T>
+void ForwardLinkedList<T>::PushBack(const T& val)
+{
+	Insert(val, (int)Count() + 1);           //调用Count函数计算结点个数，加一实现尾插
+}
+
+template<class T>
+size_t ForwardLinkedList<T>::Count()
+{
+	len = 0;                      //初始化结点计数符len
+	LNode<T>* pt = head;
+	while (pt->next != nullptr)
+	{
+		len++;                     //计算链表结点个数
+		pt = pt->next;
+	}
+	return len;
+}
+
+template<class T>
+bool ForwardLinkedList<T>::Empty()
+{
+	if (head->next == nullptr)      //头结点的next(第一个结点)为空则链表为空
+	{
+		cout << "The LinkedList is empty" << endl;
+		return 0;
+	}
+	else                           //否则不为空
+	{
+		cout << "The LinkedList isn't empty" << endl;
+		return 1;
+	}
+}
+
+template<class T>
+void ForwardLinkedList<T>::Clear()
+{
+	if (head == nullptr||head->next==nullptr)
+	{
+		return;
+	}
+	LNode<T>* pt1 = head->next;   //前指针
+	head->next = nullptr;
+	LNode<T>* pt2 = pt1;   //后指针
+	while (pt2 != nullptr)
+	{
+		pt1 = pt1->next;
+		delete(pt2);       //删除前一个结点
+		pt2 = pt1;
+	}
+	delete(pt1);         //删除末尾结点
+	cout << "Clear successfully" << endl;
+}
+
+template<class T>
+LNode<T>* ForwardLinkedList<T>::MidFinder()
+{
+	if (Empty()==0)
+	{
+		return nullptr;
+	}
+	LNode<T>* pt1 = head;     //快指针
+	LNode<T>* pt2 = pt1;      //慢指针
+	while (pt1->next != nullptr)
+	{
+		pt1 = pt1->next;
+		pt2 = pt2->next;
+		if (pt1->next != nullptr)
+			pt1 = pt1->next;
+		else break;
+	}
+	return pt2;
+}
--- Week1/Week1.cpp
+++ Week1/Week1.cpp
@@ -1,8 +1,51 @@
 ﻿#include "LinkedList.h"
 #include "ForwardLinkedList.h"
+#include <iostream>
 
+using namespace std;
+
+//刘曦烨-3122004746
 
 int main()
 {
-}
+	cout << "Now test ForwardLinkedList" << endl;
+	ForwardLinkedList<int> e;
+	e.Print();           //打印链表中的data,(跳过头结点)
+	e.PushFront(3);      //分别前插3，2，1
+	e.PushFront(2);
+	e.PushFront(1);
+	e.PushBack(6);      //分别尾插6，7，8
+	e.PushBack(7);
+	e.PushBack(8);
+	e.Insert(4, 4);     //中间再插入两个结点，插在第4，5结点
+	e.Insert(5, 5);
+	e.Print();          //打印链表内容
+	e.Erase(3);
+	e.Erase(1);
+	e.Erase((int)e.Count());
+	e.Print();
+	cout << e.MidFinder()->data << endl;     //找到中间结点(其中调用empty函数检查链表是否为空),并打印data
+	e.Clear();
+	cout << "ForwardLinkedList test over" << endl;
 
+	cout << endl;
+
+	cout << "Now test LinkedList" << endl;
+	LinkedList<int> d;
+	d.Print();           //打印链表中的data
+	d.PushFront(3);      //分别前插3，2，1
+	d.PushFront(2);
+	d.PushFront(1);
+	d.PushBack(6);      //分别尾插6，7，8
+	d.PushBack(7);
+	d.PushBack(8);
+	d.Insert(4, 4);     //中间再插入两个结点，插在第4，5结点
+	d.Insert(5, 5);
+	cout << endl;
+	d.Print();          //打印链表内容
+	cout << d.MidFinder()->data << endl;     //找到中间结点(其中调用empty函数检查链表是否为空),并打印data
+	d.Clear();
+	cout << "LinkedList test over" << endl;
+
+
+}
